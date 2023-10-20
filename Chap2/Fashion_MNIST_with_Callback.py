694f9a9e9a702d9bacdb567686da1d80f92516c4
isocy <isocy13576@kaist.ac.kr>
2023-10-20 11:51:55 +0900

    Add code

--- Chap2/Fashion_MNIST_with_Callback.py
+++ Chap2/Fashion_MNIST_with_Callback.py
@@ -1,6 +1,15 @@
 from tensorflow.keras.datasets import fashion_mnist
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Flatten, Dense
+from tensorflow.keras.callbacks import Callback
+
+
+class MyCallback(Callback):
+    def on_epoch_end(self, epochs, logs=None):
+        if logs['accuracy'] > 0.85 and logs['accuracy'] - logs['val_accuracy'] > 0.03:
+            print('\ntraining accuracy reached 0.85 '
+                  'and difference between training accuracy and validation accuracy reached 0.3')
+            self.model.stop_training = True
 
 
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
@@ -13,9 +22,6 @@ model = Sequential([
     Dense(units=10, activation='softmax')
 ])
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-model.fit(train_images, train_labels, epochs=5)
+callback = MyCallback()
+model.fit(train_images, train_labels, validation_data=(test_images, test_labels), epochs=50, callbacks=[callback])
 model.evaluate(test_images, test_labels)
-
-# classifications = model.predict(test_images)
-# print(classifications[0])
-# print(test_labels[0])
