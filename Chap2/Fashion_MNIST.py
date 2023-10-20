dcd006fb4e738ed41c05beaf7478025702fdbe62
isocy <isocy13576@kaist.ac.kr>
2023-10-20 10:39:16 +0900

    Add code

--- Chap2/Fashion_MNIST.py
+++ Chap2/Fashion_MNIST.py
@@ -1,9 +1,21 @@
+from tensorflow.keras.datasets import fashion_mnist
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Flatten, Dense
 
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+train_images = train_images / 255.0
+test_images = test_images / 255.0
 
 model = Sequential([
     Flatten(input_shape=(28, 28)),
     Dense(units=128, activation='relu'),
     Dense(units=10, activation='softmax')
 ])
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.fit(train_images, train_labels, epochs=5)
+model.evaluate(test_images, test_labels)
+
+classifications = model.predict(test_images)
+print(classifications[0])
+print(test_labels[0])
