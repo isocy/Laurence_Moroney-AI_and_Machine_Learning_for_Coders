cb0e3ed28c29d38bc02be775a0d2465eb151db77
isocy <isocy13576@kaist.ac.kr>
2023-10-26 16:51:40 +0900

    in progress

--- Chap3/horse_or_human/1_ImageDataGenerator.py
+++ Chap3/horse_or_human/1_ImageDataGenerator.py
@@ -3,17 +3,34 @@ import os
 import PIL
 import PIL.Image
 import tensorflow as tf
-import tensorflow_datasets as tfds  # remove 'import resource' from tensorflow_datasets.core.shuffle
+import tensorflow_datasets as tfds
+
+import pathlib
+import matplotlib.pyplot as plt
 
 
-print(tf.__version__)
+dataset_url = 'https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz'
+archive = tf.keras.utils.get_file(origin=dataset_url, extract=True)
+data_dir = pathlib.Path(archive).with_suffix('')
+
+image_cnt = len(list(data_dir.glob('*/*.jpg')))
+print(image_cnt)
+
+roses = list(data_dir.glob('roses/*'))
+img = PIL.Image.open(roses[0])
+plt.imshow(img)
+plt.show()
+
+img = PIL.Image.open(roses[1])
+plt.imshow(img)
+plt.show()
 
 
 # from tensorflow.keras.preprocessing.image import ImageDataGenerator
-#
-#
+# 
+# 
 # train_datagen = ImageDataGenerator(rescale=1/255)
-#
+# 
 # train_generator = train_datagen.flow_from_directory(
 #     './datasets/training/',
 #     target_size=(300, 300),
