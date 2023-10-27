68eeac192f303893cab1f4d158ab2f1f5dc21c21
isocy <isocy13576@kaist.ac.kr>
2023-10-27 11:47:14 +0900

    in progress

--- Chap3/horse_or_human/0-1_download_flower_images.py
+++ Chap3/horse_or_human/0-1_download_flower_images.py
@@ -5,4 +5,6 @@ from tensorflow.keras.utils import get_file
 
 dataset_url = 'https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz'
 file_path = get_file(origin=dataset_url, extract=True)
+dataset_path = pathlib.Path(file_path).with_suffix('')
 
+with open('./')
